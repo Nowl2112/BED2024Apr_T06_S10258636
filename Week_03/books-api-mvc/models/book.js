5384e57a45ba95526a9f3d3adbc3eccea68e00f7
Nowl2112 <s10258636@connect.np.edu.sg>
2024-05-10 10:33:36 +0800

    week03 mistake

--- Week_03/books-api-mvc/models/book.js
+++ Week_03/books-api-mvc/models/book.js
@@ -1,11 +1,3 @@
-class Book {
-  constructor(id, title, aurthor) {
-    this.id = id;
-    this.title = title;
-    this.author = this.author;
-  }
-}
-module.exports = Book;
 const books = [
   { id: 1, title: "The Lord of the Rings", author: "J.R.R. Tolkien" },
   { id: 2, title: "Pride and Prejudice", author: "Jane Austen" },
